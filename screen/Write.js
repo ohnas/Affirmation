b45d93a1fcb78c237c52e66612955772ff6b5666
ohnas <ohnas12@gmail.com>
2023-06-13 15:55:56 +0900

    Write.js

--- screen/Write.js
+++ screen/Write.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from 'styled-components/native';
 import { useDB } from '../context';
+import { Alert, Text } from 'react-native';
 
 const Container = styled.View`
   flex: 1;
@@ -8,19 +9,90 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
 `;
-const TitleBox = styled.View``;
+const TitleBox = styled.View`
+  flex: 1;
+  width: 95%;
+  align-items: center;
+  justify-content: center;
+`;
 const TitleText = styled.Text`
   font-size: 20px;
   font-weight: 600;
 `;
+const InputBox = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 95%;
+`;
+const MessageInput = styled.TextInput`
+  border-bottom-width: 2px;
+  width: 68%;
+  font-size: 20px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  text-align: center;
+`;
+const GoalInput = styled.TextInput`
+  border-bottom-width: 2px;
+  width: 28%;
+  font-size: 20px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  text-align: center;
+`;
+const AffirmaitonList = styled.View`
+  flex: 8;
+  width: 95%;
+  border: 2px;
+`;
 
 function Write() {
-  const db = useDB();
+  const realmDB = useDB();
+  const [message, setMessage] = useState("");
+  const [goal, setGoal] = useState("");
+  const onChangeMessage = (text) => setMessage(text);
+  const onChangeGoal = (num) => setGoal(num);
+  const onSubmit = () => {
+    if (message === "" || goal == "") {
+      return Alert.alert("Please complete form.");
+    } else {
+      realmDB.write(() => {
+        realmDB.create("Affirmation", {
+          message: message,
+          goal: Number(goal),
+        });
+      });
+    }
+  };
   return(
     <Container>
       <TitleBox>
         <TitleText>Affirmation List</TitleText>
       </TitleBox>
+      <InputBox>
+        <MessageInput 
+          placeholder='Affirmaiton'
+          placeholderTextColor='#8395a7'
+          onChangeText={onChangeMessage}
+          value={message} 
+          inputMode='text'
+          returnKeyType='done'
+        />
+        <GoalInput 
+          placeholder='Goal' 
+          placeholderTextColor='#8395a7'
+          onChangeText={onChangeGoal}
+          value={goal} 
+          inputMode='numeric'
+          returnKeyType='done'
+          onSubmitEditing={onSubmit}
+        />
+      </InputBox>
+      <AffirmaitonList>
+        <Text>FlatList</Text>
+      </AffirmaitonList>
     </Container>
   );
 }
